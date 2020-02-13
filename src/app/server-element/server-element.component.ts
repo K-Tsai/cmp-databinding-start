bbe8123b32a127379f3a44cc973246d409659718
Kevin Tsai <k.tsai92@gmail.com>
2020-02-13 15:47:16 -0500

    added lifecycle

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges,
+  SimpleChanges 
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  constructor() {
+    console.log('constructor called!');
+   }
 
-  constructor() { }
+   ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    console.log('ngOnInit called!');
   }
 
 }
